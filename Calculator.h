48f22dcdb51665c1fce2b6e530338f9b7ea0e786
Oleys167 <111304864+Oleys167@users.noreply.github.com>
2022-08-17 20:00:06 +0300

    добавлена документация

--- Calculator.h
+++ Calculator.h
@@ -1,9 +1,10 @@
 #include <iostream>
 
 using namespace std;
-
+//Класс калькулятор предназначен для сложения,вычитания,
+//умножения,деления,получения остатка от деления 2-х чисел
 class Calculator{
-    int A,B;
+    int A,B;//первое и второе
     char znak;
     void input(){cin >> A >> znak >> B;}
 public:
